c753e3938f20a99a8101ebf85a60a9a8a01cc4b8
Juan Esteban Galeano Herrera <jegaleanoherrera@gmail.com>
2022-10-22 11:30:43 -0500

    Avances en las operaciones CRUD de cuencas y metodos - refactorización con excepciones

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from distutils.log import error
+from msilib.schema import Error
 import eel
 import sys
 import  sqlite3 as sql
 import json
+
+from pyparsing import col
 
 
 #Conexión a la base de datos
@@ -17,73 +20,78 @@ eel.init("www")
 #Select / READ
 @eel.expose       
 def select(table_name):
+    #decoded=""
     try:
         conn = sql.connect("./DataSource/PescasArtesanalesDB.sqlite")
-    except:
-        print("Error al conectar con la base de datos")
-    cursor = conn.cursor()
-    lista_registros = []
-    for row in cursor.execute("SELECT * FROM " + table_name):
-        lista_registros.append(row)
-    conn.close()
-    encoded = json.dumps(lista_registros, ensure_ascii=False).encode('utf8')
-    decoded = encoded.decode()
+        print("Conectado a la base de datos para ejecutar select...")
+        cursor = conn.cursor()
+        lista_registros = []
+        for row in cursor.execute("SELECT * FROM " + table_name):
+            lista_registros.append(row)
+        print("Query select ejecutado...")
+
+        encoded = json.dumps(lista_registros, ensure_ascii=False).encode('utf8')
+        decoded = encoded.decode()
+    except sql.Error as error:
+        print("Error al conectar con la base de datos - ",error)
+    finally:
+        if conn:
+            conn.close()
+            print("La conexión a la base de datos ha finalizado...")
+
     return decoded
 
 @eel.expose
 def create(table_name, args):
-    column_name = ""
     
     try:
         conn = sql.connect("./DataSource/PescasArtesanalesDB.sqlite")
-    except:
-        print("Error al conectar con la base de datos")
-    cursor = conn.cursor()
-    
-    if(table_name == "metodos"):
-        try:
-            query = "INSERT INTO " + table_name + "(metodo) VALUES (?)"
-            cursor.execute(query,[args])
-            conn.commit()
-        except:
-            print("Error en query en tabla métodos")
-    elif (table_name == "cuencas"):
-        try:
-            query = "INSERT INTO " + table_name + "(cuenca) VALUES (?)"
-            cursor.execute(query,args)
-            conn.commit()
-        except:
-            print("Error en query en tabla cuencas")
-
-
-    conn.close()
+        print("Conectado a la base de datos para ejecutar create...")
+        cursor = conn.cursor()
+        columna=""
+        if(table_name=="metodos"):
+            columna = "metodo"
+        elif(table_name == "cuencas"):
+            columna="cuenca"
+            
+        query = "INSERT INTO " + table_name + " (" + columna + ") VALUES (?)"
+        cursor.execute(query,[args])
+        conn.commit()
+        print(columna+" creada satisfactoriamente")
+    except sql.Error as erros:
+        print("Error al crear registro en la base de datos - ",erros)
+    finally:
+        if conn:
+            conn.close()
+            print("La conexión a la base de datos ha finalizado...")
 
 
 
 #Update
 @eel.expose
 def update(table_name, args):
-    print(args)
     try:
         conn = sql.connect("./DataSource/PescasArtesanalesDB.sqlite")
-    except:
-        print("Error al conectar con la base de datos")
-    cursor = conn.cursor()
-    if table_name == "metodos":
-        try:
-            query = "UPDATE " + table_name + " SET metodo=(?) WHERE id_metodo=(?);"
-            cursor.execute(query, [args[1], args[0]])
-            conn.commit()
-        except:
-            print("Error al actualizar tabla métodos")
-    elif (table_name == "cuencas"):
-        try:
-            query = "UPDATE " + table_name + " SET cuenca=(?) WHERE id_cuenca=(?);"
-            cursor.execute(query, [args[1], args[0]])
-            conn.commit()
-        except:
-            print("Error al actualizar tabla cuencas")
-    conn.close()
+        cursor = conn.cursor()
+        columna=""
+        id_column=""
+        if (table_name =="metodos"):
+            columna = "metodo"
+            id_column = "id_metodo"
+        elif(table_name=="cuencas"):
+            columna="cuenca"
+            id_column = "id_cuenca"
+                
+        query = "UPDATE " + table_name + " SET "+columna+"=(?) WHERE "+id_column+"=(?);"
+        cursor.execute(query, [args[1], args[0]])
+        conn.commit()
+        print("Elemento en ", columna, "Actualizado satisfactoriamente")
+    except sql.Error as error:
+        print("Error al conectar con la base de datos", error)
+    finally:
+        if conn:
+            conn.close()
+            print("La conexión a la base de datos ha finalizado...")
     
 #Delete
 @eel.expose
@@ -93,13 +101,20 @@ def delete(table_name, args):
     except:
         print("Error al conectar con la base de datos")
     cursor = conn.cursor()
-    if table_name == "metodos" or table_name == "cuencas":
+    if table_name == "metodos":
         try:
             query = "DELETE FROM " + table_name + " WHERE id_metodo=(?);"
             cursor.execute(query, [args])
             conn.commit()
-        except:
-            print("Error al eliminar un dato en la tabla: "+table_name)
+        except sql.Error as erros:
+            print("Error al eliminar un dato en la tabla: "+table_name+" - "+error)
+    elif table_name == "cuencas":
+        try:
+            query = "DELETE FROM " + table_name + " WHERE id_cuenca=(?);"
+            cursor.execute(query, [args])
+            conn.commit()
+        except sql.Error as erros:
+            print("Error al eliminar un dato en la tabla: "+table_name+" - "+error)
     conn.close()
 
 
---
+++ www/js/cuencas.js
@@ -0,0 +1,89 @@
+// Obtiene el nombre de la tabla según el nombre del archivo
+let table_name = document.title;
+console.log("la tabla es: " + table_name);
+
+// Actualizar tablas
+function update_table() {
+    eel.select(table_name)(get_data);
+}
+
+// CREATE
+document.querySelector(".crud_create").onclick = function (){ 
+    alert("Entra al metodo");
+    create_name = document.getElementById("create_name");
+    //clean_msgs();
+    if(!create_name.value) {       
+        alert("La entrada no puede estar vacía");
+    }
+    else {
+        try {
+            eel.create(table_name,create_name.value);
+            update_table();
+            alert("Elemento agregado exitosamente");
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    clean_inputs();
+}  
+
+// READ
+window.onload = function () {
+    eel.select(table_name)(get_data);
+}
+
+function get_data(output){
+    json_list = JSON.parse(output);
+    string_table = "<tr><th>Id método</th><th>Nombre del método</th></tr>";    
+    string_select = "<option disabled selected value style='color:whitesmoke'></option>";
+    json_list.forEach(row => string_table = string_table.concat("<tr><td>", row[0], "</td>", "<td>", row[1] ,"</td></tr>"));
+    json_list.forEach(row => string_select = string_select.concat("<option value='", row[0], "'>", row[0], " - ", row[1], "</option>"));
+    document.getElementById("data").innerHTML = string_table;
+    document.getElementById("update_id").innerHTML = string_select;
+    document.getElementById("delete_id").innerHTML = string_select;
+}
+
+//UPDATE
+document.querySelector(".crud_update").onclick = function (){ 
+    update_id = document.getElementById("update_id");
+    update_new_name = document.getElementById("update_name");
+    update_args = [update_id.value, update_new_name.value];
+    //clean_msgs();
+    if(!update_args[0] || !update_args[1]) {
+        alert("La entrada no puede estar vacía");
+    }
+    else {
+        try {
+            eel.update(table_name, update_args);
+            update_table();
+            alert("Elemento actualizado exitosamente");
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    clean_inputs();
+} 
+
+//DELETE
+document.querySelector(".crud_delete").onclick = function (){ 
+    delete_id = document.getElementById("delete_id");
+
+    //clean_msgs();
+    if(!delete_id.value){
+        alert("Debe seleccionar una opción");
+    }
+    else {
+        eel.delete(table_name, delete_id.value);
+        update_table();
+        alert("Elemento eliminado exitosamente");
+    }
+    clean_inputs();
+}
+
+//Limpiar inputs
+function clean_inputs() {
+    inputs = document.getElementsByClassName("crud_input");
+    selects = document.getElementsByClassName("crud_select");
+    for (let i of inputs) { i.value = ""; }
+    for (let s of selects) { s.value = ""; }
+}
